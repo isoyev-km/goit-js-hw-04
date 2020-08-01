8ec4625a1427511bb8318144b27836c3c2b58980
Kamil <60015567+isoyev-km@users.noreply.github.com>
2020-08-01 14:53:39 +0300

    start

---
+++ task-1-hw-4.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Петя бежит быстро, потому что Петя пытается поймать поезд.
+const petya = {
+    name: 'Petya',
+    showName() {
+      console.log(petya.name);
+    },
+  };
+  
+  petya.showName();
+  
